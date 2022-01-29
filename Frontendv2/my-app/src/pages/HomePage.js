bedc87b0d53be7fb6e3394acd70c10e2cfb63238
reemo <r48n34@gmail.com>
2022-01-29 19:07:43 +0800

    Update HomePage.js

--- Frontendv2/my-app/src/pages/HomePage.js
+++ Frontendv2/my-app/src/pages/HomePage.js
@@ -3,20 +3,26 @@ import { useEffect } from "react";
 //import { useNavigate } from "react-router-dom";
 
 import { basicCallSwal } from './utility/swalCall'
-
+import moment from 'moment'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 function TodayAttendentsChar(){
+
+    const attendNum = 30;
+    const absentNum = 2;
+    const classCurrent = "3A"
+    
     const data = {
         labels: ['Absent', 'Attend'],
         datasets: [
           {
             label: 'numbers of attended',
-            data: [30, 2],
+            data: [absentNum, attendNum],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -33,8 +39,8 @@ function TodayAttendentsChar(){
 
     return(
         <Card>
-            <Card.Title style={{ textAlign:"center", paddingTop:"8px" }}> Today attendents: </Card.Title>
-            <Doughnut data={data}/>
+            <Card.Title style={{ textAlign:"center", paddingTop:"8px" }}> Today ({ moment().format() }) {classCurrent} attendents: </Card.Title>
+            <Doughnut data={data}/> 
             <br/>
         </Card>
     )
@@ -58,6 +64,10 @@ function HomePage(){
 
             <Row>
                 <Col md={6} lg={4}>
+                    <TodayAttendentsChar/>
+                </Col>
+
+                <Col md={6} lg={8}>
                     <TodayAttendentsChar/>
                 </Col>
 
